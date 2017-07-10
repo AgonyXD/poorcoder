39f945ad84e6a7acac51aa64dfc8d83f2b413aea
root <root@bogon.(none)>
2017-07-10 15:30:31 +0800

    modify routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', function ()    {
+        return 'admin';
+    });
+});
